403ed7136d857453cf114878fb2fead5e673e1d1
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-24 16:17:15 +0300

    add borrowing admin configuration

---
+++ borrowings/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from borrowings.models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "book",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
+    list_filter = ("borrow_date",)
+    search_fields = ("user__email", "book__title")
