2277ebbbc95b734929ac7a6ac03a5fe5743b3d48
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-24 16:16:40 +0300

    add borrowing serializer and read-only viewset and connect urls

---
+++ borrowings/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from borrowings.views import BorrowingViewSet
+
+router = DefaultRouter()
+router.register("", BorrowingViewSet, basename="borrowings")
+
+urlpatterns = router.urls
